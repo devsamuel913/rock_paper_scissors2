4de8769dfd604a25d64551e2802b3c3fc691a943
Nathan Madrid <devsamuelescobar913@gmail.com>
2024-06-26 22:22:42 -0600

    Selectors
    I established selectors for the event listeners

---
+++ main.js
@@ -0,0 +1,14 @@
+//Selectors
+const rockButton = document.getElementById("#rock_id");
+const paperButton = document.getElementById("#paper_id");
+const scissorsButton = document.getElementById("#scissors_id");
+
+//Variables
+let humanScore = 0;
+let cpuScore = 0;
+
+
+//Event listeners
+rockButton.addEventListener("click",)
+
+//Functions
